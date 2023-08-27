290d1885c1efb187292aa28f2f1d54f582821fc0
hello-developer-sanjay <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-08-27 21:01:30 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = ({ activeButton, onSetActiveButton, setSelectedDocument, setSelec
 
    const fetchData = (collection) => {
     axios
-      .get(`https://uy5nvg.onrender.com/api/${collection}`)
+      .get(`https://needme.onrender.com/api/${collection}`)
       .then((response) => {
         console.log(`Response data for ${collection} collection:`, response.data);
   
@@ -44,7 +44,7 @@ const Header = ({ activeButton, onSetActiveButton, setSelectedDocument, setSelec
 
   const fetchDocumentData = (collection, title) => {
     axios
-      .get(`https://uy5nvg.onrender.com/api/${collection}?title=${encodeURIComponent(title)}`)
+      .get(`https://needme.onrender.com/api/${collection}?title=${encodeURIComponent(title)}`)
       .then((response) => {
         setSelectedDocument(response.data.find((item) => item.title === title));
         setSelectedCollectionAndTitle({ collection, title });
