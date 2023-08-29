9242f760279d6966e37d0293ac9597d8924fac5c
hello-developer-sanjay <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-08-29 20:38:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
 
   const fetchDocumentData = (collection, title) => {
     axios
-      .get(`https://back-ox05.onrender.com/api/${collection}?title=${encodeURIComponent(title)}`)
+      .get(`https://hello-back-0iam.onrender.com/api/${collection}?title=${encodeURIComponent(title)}`)
       .then((response) => {
         setSelectedDocument(response.data.find((item) => item.title === title));
         setSelectedCollectionAndTitle({ collection, title });
